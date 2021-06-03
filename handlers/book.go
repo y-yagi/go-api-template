4e567c81a281823dc4110c790ee763f1c9b0dd5a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-06-03 13:35:10 +0900

    Implement CRUD

--- handlers/book.go
+++ handlers/book.go
@@ -1,21 +1,54 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/y-yagi/go-api-template/database"
+	"github.com/y-yagi/go-api-template/ent"
+	"github.com/y-yagi/go-api-template/ent/book"
 )
 
 func GetBooks(c *fiber.Ctx) error {
-	return nil
+	books, err := database.Client.Book.Query().All(c.UserContext())
+	if err != nil {
+		return err
+	}
+	return c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) error {
-	return nil
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	book, err := database.Client.Book.Query().Where(book.ID(id)).Only(c.UserContext())
+	if err != nil {
+		return err
+	}
+	return c.JSON(book)
 }
 
 func CreateBook(c *fiber.Ctx) error {
-	return nil
+	book := new(ent.Book)
+	if err := c.BodyParser(&book); err != nil {
+		return err
+	}
+
+	_, err := database.Client.Book.Create().SetName(book.Name).SetAuthor(book.Author).Save(c.UserContext())
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(book)
 }
 
 func DeleteBook(c *fiber.Ctx) error {
-	return nil
+	id, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	return database.Client.Book.DeleteOneID(id).Exec(c.UserContext())
 }
