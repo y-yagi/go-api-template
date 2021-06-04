f05fda0688cf54f5643229be851bc9252a238d57
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-06-04 16:38:36 +0900

    Show a request id in log

--- routes/routes.go
+++ routes/routes.go
@@ -5,13 +5,17 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/y-yagi/go-api-template/handlers"
+
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func New() *fiber.App {
 	app := fiber.New()
+	app.Use(requestid.New())
 	app.Use(cors.New())
 	app.Use(logger.New(logger.Config{
-		Format:     "${cyan}[${ip}] ${blue}${status} ${blue}[${method}] ${white}${path} at ${white}${time}\n",
+		Format: "[${locals:requestid}] ${cyan}[${ip}] ${blue}${status} ${blue}[${method}] ${white}${path} at ${white}${time}\n",
+
 		TimeFormat: "2006-01-02 15:04:05 +09:00",
 	}))
 
